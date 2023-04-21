3efb3c30c5fec615ffc3a79710eb2054fa440045
luciadesperati <126161614+luciadesperati@users.noreply.github.com>
2023-04-21 14:58:57 +0100

    JS added and moment library imported

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,27 @@
 // alert("Welcome");
+
+function updateTime() {
+  // Brighton
+  let brightonElement = document.querySelector("#brighton");
+  let brightonDateElement = brightonElement.querySelector(".date");
+  let brightonTimeElement = brightonElement.querySelector(".time .time-hours");
+  let brightonAmPmElement = brightonElement.querySelector(".time .am-pm");
+  let brightonTimezone = moment().tz("Europe/London");
+  brightonDateElement.innerHTML = brightonTimezone.format("MMMM Do YYYY");
+  brightonTimeElement.innerHTML = brightonTimezone.format("h:mm:ss");
+  brightonAmPmElement.innerHTML = brightonTimezone.format("A");
+
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement =
+    losAngelesElement.querySelector(".time .time-hours");
+  let losAngelesAmPmElement = losAngelesElement.querySelector(".time .am-pm");
+  let losAngelesTimezone = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTimezone.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTimezone.format("h:mm:ss");
+  losAngelesAmPmElement.innerHTML = losAngelesTimezone.format("A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
